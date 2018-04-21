85c73bf4c3df344a9f6fe40209e33eedda2d6dc1
Md. Amran Hossain <helloimran24h@gmail.com>
2018-04-21 13:20:19 +0000

    Update break tag replace.java
    ultimate code

--- break tag replace.java
+++ break tag replace.java
@@ -10,17 +10,23 @@ import org.jsoup.safety.Whitelist;
 public class JavaApplication19 {
 
     public static void main(String[] args) {
-      String strings= "<div>imran imranimranimranimranimran imranimran<br/><br/><br/>imranimran</div>";
- 
-    
-    String s="wefjwefhiwf\n\n\njwfvuwfbw\n\nuioefhcweiuf";
-    s=s.trim().replaceAll("\n+","\n");//+ for multiple occurrences 
-    System.out.println(s);
-    String s1="wefjwefhiwf<br/><br/><br/>jwfvuwfbw<br/><br/>uioefhcweiuf";
-    s1=s1.trim().replaceAll("<br/>","\n");//+ for multiple occurrences 
+        
+        
+//    String strings= "<div>imran imranimranimranimranimran imranimran<br/><br/><br/>imranimran</div>";
+//    String s="wefjwefhiwf\n\n\njwfvuwfbw\n\nuioefhcweiuf";
+//    s=s.trim().replaceAll("\n+","\n");//+ for multiple occurrences 
+//    System.out.println(s);
+//    String s1="wefjwefhiwf<br/><br/><br/>jwfvuwfbw<br/><br/>uioefhcweiuf";
+//    s1=s1.trim().replaceAll("<br/>","\n");//+ for multiple occurrences 
+//    System.out.println(s1);
+//    String s2=s1.trim().replaceAll("\n+","\n");
+//    System.out.println(s2);
+        
+    String s1="wefjwefhiwf<br/><br/><br/><br><br>jwfvuwfbw<br/><br/><br/><br/><br/>uioefhc<br/><br/><br/>weiuf";
+    s1=s1.trim().replaceAll("(?i)<br[^>]*>","\n");//+ for multiple occurrences 
     System.out.println(s1);
-    String s2=s1.trim().replaceAll("\n+","\n");
-System.out.println(s2);
+    String s2=s1.trim().replaceAll("\n+","<br/><br/>");
+    System.out.println(s2);
     }
     
 }
